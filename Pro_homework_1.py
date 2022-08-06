47128928be09422e3bf2dbf4c24f040e2e2b5325
Gydermes <gydermes23@gmail.com>
2022-08-06 23:50:04 +0300

    first commit

--- Pro_homework_1.py
+++ Pro_homework_1.py
@@ -1,25 +1,18 @@
-class Phone:
+class Shop:
 
-    def __init__(self, model, color, year):
+    def __init__(self, technique, model, price):
 
+        self.technique = technique
         self.model = model
-        self.color = color
-        self.year = year
+        self.price = price
 
     def __str__(self):
 
-        return f'{self.model}, {self.color}, {self.year}'
-
-
-phone_1 = Phone("Iphone", "white", 2020)
-phone_2 = Phone("Samsung", "red", 2019)
-
-print(phone_1, phone_2, sep="\n")
+        return f'{self.technique}, {self.model},{self.price}$'
 
 
 class Buyer:
     def __init__(self, surname, name, patronymic, phone_number):
-
         self.surname = surname
         self.name = name
         self.patronymic = patronymic
@@ -27,9 +20,11 @@ class Buyer:
 
     def __str__(self):
 
-        return f'{self.surname}, {self.name}, {self.patronymic}, {self.phone_number}'
+        return f'{self.surname} {self.name[0]}.{self.patronymic[0]}., {self.phone_number}'
 
 
+product_1 = Shop("Phone", "Iphone", 200)
+product_2 = Shop("TV", "Samsung", 105)
 buyer_1 = Buyer("Ivanov", "Ivan", "Ivanovich", "+380933344444")
 
-print(buyer_1)
+print(buyer_1, "Product :", product_1, product_2, sep="\n")
