bc894f5e18c6b2e7b92f914b92135941c7c3ac8f
Gydermes <110401357+Gydermes@users.noreply.github.com>
2022-08-14 14:19:53 +0300

    Add files via upload

--- Pro_homework_1.py
+++ Pro_homework_1.py
@@ -1,30 +1,86 @@
-class Shop:
 
-    def __init__(self, technique, model, price):
+class Product:
 
-        self.technique = technique
-        self.model = model
+    def __init__(self, title: str, price: int | float):
+        if not isinstance(title, str):
+            raise TypeError()
+        self.title = title
+        if not isinstance(price, int | float):
+            raise TypeError()
         self.price = price
 
     def __str__(self):
+        return f'{self.title}: {self.price} грн.'
 
-        return f'{self.technique}, {self.model},{self.price}$'
 
+class Customer:
 
-class Buyer:
-    def __init__(self, surname, name, patronymic, phone_number):
-        self.surname = surname
+    def __init__(self, name: str, surname: str, phone: str):
+        if not isinstance(name, str):
+            raise TypeError()
         self.name = name
-        self.patronymic = patronymic
-        self.phone_number = phone_number
+        if not isinstance(surname, str):
+            raise TypeError()
+        self.surname = surname
+        if not isinstance(phone, str):
+            raise TypeError()
+        self.phone = phone
 
     def __str__(self):
+        return f'{self.surname} {self.name[0]}., {self.phone}'
 
-        return f'{self.surname} {self.name[0]}.{self.patronymic[0]}., {self.phone_number}'
+
+class Cart:
+
+    def __init__(self, customer: Customer = None):
+        self.customer = customer
+        self.products = []
+        self.quantities = []
+
+    def add_product(self, product: Product, quantity: int | float):
+        if product in self.products:
+            index = self.products.index(product)
+            self.quantities[index] += quantity
+        else:
+            self.products.append(product)
+            self.quantities.append(quantity)
+
+    def total(self):
+        res = 0
+        for index, item in enumerate(self.products):
+            res += item.price * self.quantities[index]
+        return res
+
+    def __str__(self):
+        res = f'{self.customer}\n'
+
+        for index, item in enumerate(self.products):
+            res += f'\t{item} x {self.quantities[index]} = {item.price * self.quantities[index]} грн.\n'
+
+        res += f'Total price: {self.total()} грн.'
+
+        return res
 
 
-product_1 = Shop("Phone", "Iphone", 200)
-product_2 = Shop("TV", "Samsung", 105)
-buyer_1 = Buyer("Ivanov", "Ivan", "Ivanovich", "+380933344444")
+pr_1 = Product('banana', 45)
+pr_2 = Product('apple', 21)
+pr_3 = Product('orange', 22)
 
-print(buyer_1, "Product :", product_1, product_2, sep="\n")
+customer_1 = Customer('Ivan', 'Ivanov', '123456789')
+customer_2 = Customer('Ivan', 'Petrov', '223456789')
+
+order_1 = Cart(customer_1)
+order_2 = Cart(customer_2)
+
+order_1.add_product(pr_1, 2.5)
+order_1.add_product(pr_2, 3.)
+order_1.add_product(pr_1, 2)
+order_2.add_product(pr_1, 1)
+order_2.add_product(pr_2, 1)
+order_2.add_product(pr_3, 1)
+
+# print(order_1)
+print(order_1.customer)
+print(order_1)
+print(order_1.quantities)
+# print(order_2)
--- Pro_homework_2.py
+++ Pro_homework_2.py
@@ -1,73 +1,83 @@
+import settings
 
-class Human:
 
-    def __init__(self,  surname: str, name: str, patronymic: str, year_of_birth: str):
+class Person:
+
+    def __init__(self, surname, name):
+        if not isinstance(surname, str):
+            raise TypeError()
+        if not isinstance(name, str):
+            raise TypeError()
         self.surname = surname
         self.name = name
-        self.patronymic = patronymic
-        self.year_of_birt = year_of_birth
 
     def __str__(self):
-        return f'{self.surname} {self.name[0]}.{self.patronymic[0]}.,{self.year_of_birt}'
+        return f'{self.surname} {self.name}'
 
 
-class Student(Human):
+class Student(Person):
 
-    def __init__(self, surname, name, patronymic, year_of_birth, passport, number):
-        super().__init__(surname, name, patronymic, year_of_birth)
-        self.passport = passport
-        self.number = number
+    def __init__(self, surname: str, name: str, date_of_birth: str):
+        super().__init__(surname, name)
+        if not isinstance(date_of_birth, str):
+            raise TypeError()
+        self.date_of_birth = date_of_birth
+
+    def __eq__(self, other):
+        return self.surname == other.surname and self.name == other.name and self.date_of_birth == other.date_of_birth
 
     def __str__(self):
-        return f' passport{self.passport}, st_number: {self.number}'
+        return f'{super().__str__()}; {self.date_of_birth}'
 
 
-class Group(Student):
-    def __init__(self, surname: str, name: str, patronymic: str, year_of_birth: str, pas, tik,):
-        super().__init__(surname, name, patronymic, year_of_birth, pas, tik)
-        self.group = []
+class MaxStudentsError(Exception):
+
+    def __init__(self, msg):
+        super().__init__()
+        self.msg = msg
+
+    def __str__(self):
+        return f'{self.msg}\n{super().__str__()}'
+
+
+class Group:
+
+    def __init__(self, title):
+        self.title = title
+        self.students = []
 
     def add_student(self, student: Student):
-        group = []
-        if student in self.student:
-            self.group.append(student)
-        if student == student:
-            print(group)
-        else:
-            return 1
+        if len(self.students) >= settings.MAX_STUDENTS_IN_GROUP:
+            raise MaxStudentsError('Too many student in group!')
+
+        for item in self.students:
+            if item == student:
+                return -2
+
+        self.students.append(student)
+
+    def search(self, surname):
+        for item in self.students:
+            if item.surname.lower() == surname.lower():
+                return item
+
+        return ('С такой фамилией студента нет!')
+
+    def __str__(self):
+        return f'{self.title}\n' + '\n'.join(map(str, self.students))
 
 
-human_1 = Human('Ivanov', 'Andreu', 'Ivanovich', '2005')
-human_2 = Human('Petrov', 'Petr', 'Petrovich', '2004')
-human_3 = Human('Sidorov', 'Ivan', 'Ivanovich', '1988')
-human_4 = Human('Sherbina', 'Sergei', 'Petrovich', '2000')
-human_5 = Human('Ivashko', 'Ivan', 'Andreevich', '2002')
-human_6 = Human('Sherbina', 'Olga', 'Petrovna', '2001')
-human_7 = Human('Shevchenko', 'Georgiu', 'Valentinovich', '2000')
-human_8 = Human('Abramovich', 'Nata', 'Petrovna', '2004')
-human_9 = Human('Lusenko', 'Zlata', 'Ivanovna', '2003')
-human_10 = Human('Shevchenko', 'Alex', 'Petrovich', '2004')
+st_1 = Student('Ivanov', 'Ivan', '12-12-12')
+st_2 = Student('Ivanov1', 'Ivan1', '12-12-12')
+st_3 = Student('Ivanov1', 'Ivan2', '12-12-12')
+st_4 = Student('Ivanov', 'Ivan3', '12-12-12')
+st_5 = Student('Ivanov', 'Ivan3', '12-12-12')
 
+gr = Group('234234')
+gr.add_student(st_1)
+gr.add_student(st_2)
+gr.add_student(st_3)
+gr.add_student(st_4)
 
-student_1 = Student('Ivanov', 'Andreu', 'Ivanovich', '2005', '254s94', '24824')
-student_2 = Student('Petrov', 'Petr', 'Petrovich', '2004', '254s94', '24824')
-student_3 = Student('Sidorov', 'Ivan', 'Ivanovich', '1988', '858s97', '24834')
-student_4 = Student('Sherbina', 'Sergei', 'Petrovich', '2000', '954s94', '24844')
-student_5 = Student('Ivashko', 'Ivan', 'Andreevich', '2002', '222s94', '24854')
-student_6 = Student('Sherbina', 'Olga', 'Petrovna', '2001', '254s94', '24864')
-student_7 = Student('Shevchenko', 'Georgiu', 'Valentinovich', '2000','148s94', '24874')
-student_8 = Student('Abramovich', 'Nata', 'Petrovna', '2004', '224s94', '24884')
-student_9 = Student('Lusenko', 'Zlata', 'Ivanovna', '2003', '217s94', '24894')
-student_10 = Student('Shevchenko', 'Alex', 'Petrovich', '2004','858s94', '24850')
-
-print(human_1, student_1)
-print("2.", student_2)
-print("5.", student_3)
-print("4.", student_4)
-print("5.", student_5)
-print("6.", student_6)
-print("7.", student_7)
-print("8.", student_8)
-print("9.", student_9)
-print("10.", student_10)
-
+print(gr)
+print("search = ", gr.search("Ivan"))
