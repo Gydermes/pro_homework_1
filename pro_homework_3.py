76d8ebe69b3d931a54d774360e8dee1bc844d7d3
Gydermes <110401357+Gydermes@users.noreply.github.com>
2022-08-18 19:26:37 +0300

    Add files via upload

--- pro_homework_3.py
+++ pro_homework_3.py
@@ -57,5 +57,68 @@ print("n x = ", rec_4)
 
 # exercise 2
 
+import math
+
+
+class Rational:
+
+    def __init__(self, a, b):
+        self.a = a
+        if not isinstance(a, int):
+            raise TypeError()
+        self.b = b
+        if not isinstance(b, int):
+            raise TypeError()
+
+    def __str__(self):
+        return f'{self.a} / {self.b}'
+
+    def __add__(self, other):
+        tmp = f'{self.a + other.a} / {self.b + other.b}'
+        return tmp
+
+    def __sub__(self, other):
+        tmp = f'{self.a - other.a} / {self.b - other.b}'
+        return tmp
+
+    def __mul__(self, other):
+        tmp = f'{self.a * other.a} / {self.b * other.b}'
+        return tmp
+
+    def __truediv__(self, other):
+        tmp = f'{self.a * other.b} / {self.b * other.a}'
+        return tmp
+
+    def value(self):
+        return self.a * self.b
+
+    def __lt__(self, other):
+        return self.value() < other.value()
+
+    def __le__(self, other):
+        return self.value() <= other.value()
+
+    def __gt__(self, other):
+        return self.value() > other.value()
+
+    def __ge__(self, other):
+        return self.value() >= other.value()
+
+    def __common_denominator__(self, other):
+        w = math.gcd(int(f'{self.a + other.a} / {self.b + other.b}'))
+        return w
+
+
+x = Rational(5, 9)
+y = Rational(9, 13)
+print("x+y = ", x + y)
+print("x-y = ", x - y)
+print("x*y = ", x * y)
+print("x/y = ", x / y)
+print("x>y", x > y)
+print("x<y", x < y)
+
+
+
 
 
