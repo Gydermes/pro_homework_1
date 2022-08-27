803fcf53ad4b52e2f00a93dd4bd1d02bb120f073
Gydermes <gydermes23@gmail.com>
2022-08-27 17:13:43 +0300

    first commit

---
+++ volllllllll.py
@@ -0,0 +1,19 @@
+def gen_func(start, stop):
+    index = start
+    while index < stop:
+        yield index
+        index += 1
+
+
+git init
+
+
+y = gen_func(10, 20)
+print(y)
+for item in x:
+    print(list(x))
+
+for item in y:
+    print(list(y))
+
+
